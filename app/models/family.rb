77372c84a025da846c5f7d13d3a794d3116a2ac5
Joel Cano <joel.cano.avila@gmail.com>
2015-08-25 02:26:47 -0500

    bcrypt, er diagram init and familiy model

---
+++ app/models/family.rb
@@ -0,0 +1,5 @@
+class Family < ActiveRecord::Base
+
+  validates :name, precence: true
+
+end
---
+++ test/models/family_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FamilyTest < ActiveSupport::TestCase
+  test "should not save without name" do
+     assert true
+  end
+end
